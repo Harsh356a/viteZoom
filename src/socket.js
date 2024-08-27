00745d64a6c01122c542b36eb4f3ce82d18358dc
Harsh356a <harsh@365aitech.com>
2024-08-27 17:18:15 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-const sockets = io('https://serverzoom-mpbv.onrender.com/');
+const sockets = io('https://serverzoom-1.onrender.com/');
 export default sockets;
