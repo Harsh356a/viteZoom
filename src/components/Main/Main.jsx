0f2de2119dfb1cca2eeb15c0b0f4274841fb28b0
Harsh356a <harsh@365aitech.com>
2024-09-05 03:34:58 +0530

    harsh

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -43,21 +43,6 @@ const Main = () => {
     };
   }, [navigate]);
 
-  function clickJoin() {
-    const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
-    const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
-
-    if (!roomName || !userName) {
-      setErr(true);
-      setErrMsg("Enter Room Name and User Name");
-    } else {
-      setErr(false);
-      socket.emit("BE-join-room", { roomId: roomName, userName });
-      sessionStorage.setItem("user", userName);
-      navigate(`/room/${roomName}`);
-    }
-  }
-
   function addParticipant() {
     const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
     const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
@@ -94,69 +79,102 @@ const Main = () => {
     }
   }
 
-  function removeParticipant() {
-    const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
-    const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
-
-    if (!roomName || !userName) {
-      setErr(true);
-      setErrMsg("Enter Room Name and User Name");
-    } else {
-      fetch("https://serverzoom-mpbv.onrender.com/api/removeUser", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ roomId: roomName, userName }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.error) {
-            setErr(true);
-            setErrMsg(data.error);
-          } else {
-            setErr(false);
-            setErrMsg("");
-          }
-        })
-        .catch((error) => {
-          setErr(true);
-          setErrMsg("Error removing participant");
-        });
-    }
-  }
-
   return (
-    <div className="">
-      <div>
-        <label htmlFor="roomName">Room Name</label>
-        <input type="text" id="roomName" ref={roomRef} />
-      </div>
-      <div>
-        <label htmlFor="userName">User Name</label>
-        <input
+    <Container>
+      <Form>
+        <Label htmlFor="roomName">You are Joining :</Label>
+        <StyledInput
+          type="text"
+          id="roomName"
+          ref={roomRef}
+          readOnly
+          placeholder="Enter Room Name"
+        />
+      </Form>
+      <Form>
+        <Label htmlFor="userName">Your Name is :</Label>
+        <StyledInput
           type="text"
           id="userName"
+          readOnly
           ref={userRef}
           defaultValue={userNameFromQuery} // Pre-fill the username from the query
+          placeholder="Enter Your Name"
         />
-      </div>
-      <div>
-        <div onClick={clickJoin}>Join</div>
-        <div onClick={addParticipant}>Add Participant</div>
-        <div onClick={removeParticipant}>Remove Participant</div>
-      </div>
-      {err ? <Error>{errMsg}</Error> : null}
-      <div>
-        <h3>Participants:</h3>
-        <ul>
-          {participants.map((participant, index) => (
-            <li key={index}>{participant.info.userName}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+      </Form>
+      <ButtonContainer>
+        <JoinButton onClick={addParticipant}>Join Meeting</JoinButton>
+      </ButtonContainer>
+      {err && <Error>{errMsg}</Error>}
+    </Container>
   );
 };
+
+// Styled Components
+const Container = styled.div`
+  background-color: black;
+  width: 100%;
+  color: white;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Form = styled.div`
+  margin: 20px 0;
+`;
+
+const Label = styled.label`
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  display: block;
+  color: #f6c300;
+`;
+
+const StyledInput = styled.input`
+  background: none;
+  border: none;
+  border-bottom: 2px solid #f6c300;
+  padding: 10px 5px;
+  color: white;
+  font-size: 1rem;
+  outline: none;
+  transition: border-color 0.3s ease;
+
+  ::placeholder {
+    color: #f6c300;
+  }
+
+  &:focus {
+    border-bottom: 2px solid orange;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const JoinButton = styled.button`
+  background-color: orange;
+  color: black;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #ff9f00;
+  }
+`;
+
+const Error = styled.div`
+  color: #e85a71;
+  margin-top: 20px;
+  font-size: 1.2rem;
+`;
 
 export default Main;
