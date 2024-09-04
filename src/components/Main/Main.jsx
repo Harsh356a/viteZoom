6f50607b671b1009aa875f18e87d3ff45f5d09e6
Harsh356a <harsh@365aitech.com>
2024-09-05 03:22:58 +0530

    harsh

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
 import socket from "../../socket";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Main = () => {
   const roomRef = useRef();
@@ -12,6 +12,11 @@ const Main = () => {
   const [participants, setParticipants] = useState([]);
 
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Get userName from query params
+  const queryParams = new URLSearchParams(location.search);
+  const userNameFromQuery = queryParams.get("userName") || ""; // Fallback to empty string if not present
 
   useEffect(() => {
     socket.on("FE-error-user-exist", ({ error }) => {
@@ -39,8 +44,8 @@ const Main = () => {
   }, [navigate]);
 
   function clickJoin() {
-    const roomName = roomRef.current.value;
-    const userName = userRef.current.value;
+    const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
+    const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
 
     if (!roomName || !userName) {
       setErr(true);
@@ -54,22 +59,22 @@ const Main = () => {
   }
 
   function addParticipant() {
-    const roomName = roomRef.current.value;
-    const userName = userRef.current.value;
+    const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
+    const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
 
     if (!roomName || !userName) {
       setErr(true);
       setErrMsg("Enter Room Name and User Name");
     } else {
-      fetch('https://serverzoom-mpbv.onrender.com/api/addUser', {
-        method: 'POST',
+      fetch("https://serverzoom-mpbv.onrender.com/api/addUser", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ roomId: roomName, userName }),
       })
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           if (data.error) {
             setErr(true);
             setErrMsg(data.error);
@@ -82,7 +87,7 @@ const Main = () => {
             navigate(`/room/${roomName}`);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           setErr(true);
           setErrMsg("Error adding participant");
         });
@@ -90,22 +95,22 @@ const Main = () => {
   }
 
   function removeParticipant() {
-    const roomName = roomRef.current.value;
-    const userName = userRef.current.value;
+    const roomName = roomRef.current.value || "DefaultRoom"; // Use default room name if empty
+    const userName = userRef.current.value || userNameFromQuery; // Use userName from query if not filled
 
     if (!roomName || !userName) {
       setErr(true);
       setErrMsg("Enter Room Name and User Name");
     } else {
-      fetch('https://serverzoom-mpbv.onrender.com/api/removeUser', {
-        method: 'POST',
+      fetch("https://serverzoom-mpbv.onrender.com/api/removeUser", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ roomId: roomName, userName }),
       })
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           if (data.error) {
             setErr(true);
             setErrMsg(data.error);
@@ -114,7 +119,7 @@ const Main = () => {
             setErrMsg("");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           setErr(true);
           setErrMsg("Error removing participant");
         });
@@ -122,111 +127,36 @@ const Main = () => {
   }
 
   return (
-    <MainContainer>
-      <Row>
-        <Label htmlFor="roomName">Room Name</Label>
-        <Input type="text" id="roomName" ref={roomRef} />
-      </Row>
-      <Row>
-        <Label htmlFor="userName">User Name</Label>
-        <Input type="text" id="userName" ref={userRef} />
-      </Row>
-      <ButtonContainer>
-        <JoinButton onClick={clickJoin}>Join</JoinButton>
-        <AddButton onClick={addParticipant}>Add Participant</AddButton>
-        <RemoveButton onClick={removeParticipant}>Remove Participant</RemoveButton>
-      </ButtonContainer>
+    <div className="">
+      <div>
+        <label htmlFor="roomName">Room Name</label>
+        <input type="text" id="roomName" ref={roomRef} />
+      </div>
+      <div>
+        <label htmlFor="userName">User Name</label>
+        <input
+          type="text"
+          id="userName"
+          ref={userRef}
+          defaultValue={userNameFromQuery} // Pre-fill the username from the query
+        />
+      </div>
+      <div>
+        <div onClick={clickJoin}>Join</div>
+        <div onClick={addParticipant}>Add Participant</div>
+        <div onClick={removeParticipant}>Remove Participant</div>
+      </div>
       {err ? <Error>{errMsg}</Error> : null}
-      <ParticipantList>
+      <div>
         <h3>Participants:</h3>
         <ul>
           {participants.map((participant, index) => (
             <li key={index}>{participant.info.userName}</li>
           ))}
         </ul>
-      </ParticipantList>
-    </MainContainer>
+      </div>
+    </div>
   );
 };
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Row = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  margin-top: 15px;
-  line-height: 35px;
-`;
-
-const Label = styled.label``;
-
-const Input = styled.input`
-  width: 150px;
-  height: 35px;
-  margin-left: 15px;
-  padding-left: 10px;
-  outline: none;
-  border: none;
-  border-radius: 5px;
-`;
-
-const Error = styled.div`
-  margin-top: 10px;
-  font-size: 20px;
-  color: #e85a71;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 35px;
-`;
-
-const Button = styled.button`
-  height: 40px;
-  outline: none;
-  border: none;
-  border-radius: 15px;
-  color: #d8e9ef;
-  font-size: 25px;
-  font-weight: 500;
-  cursor: pointer;
-`;
-
-const JoinButton = styled(Button)`
-  background-color: #4ea1d3;
-  &:hover {
-    background-color: #7bb1d1;
-  }
-`;
-
-const AddButton = styled(Button)`
-  background-color: #4caf50;
-  &:hover {
-    background-color: #45a049;
-  }
-`;
-
-const RemoveButton = styled(Button)`
-  background-color: #f44336;
-  &:hover {
-    background-color: #d32f2f;
-  }
-`;
-
-const ParticipantList = styled.div`
-  margin-top: 20px;
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    margin-bottom: 5px;
-  }
-`;
 
 export default Main;
